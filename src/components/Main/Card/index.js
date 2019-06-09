5bc7ad68666f598784c2cac65d2316a06d4e428c
Wesley Folz <wesleyfolz@Wesleys-MacBook-Pro.local>
2019-06-09 12:47:46 -0600

    Added editing and saving player cards.

--- src/components/Main/Card/index.js
+++ src/components/Main/Card/index.js
@@ -1,12 +1,98 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./styles";
 
+const ENDPOINT = "http://localhost:3008/";
+
 const Card = props => {
+    const [initalPlayerInfo, setInitialPlayerInfo] = useState(props.player);
+    const [playerInfo, setPlayerInfo] = useState(props.player);
+    const [editing, setEditing] = useState(false);
+    const [teamName, setTeamName] = useState(null);
+
+    const findTeamName = () => {
+        console.log('findTeamName');
+        const filteredTeam = props.teams.filter(t => {
+            return t.id === playerInfo.team
+        });
+        console.log(filteredTeam);
+        if (filteredTeam.length) {
+            setTeamName(filteredTeam[0].name);
+        } else {
+            setTeamName('No Team');
+        }
+    };
+
+    useEffect(() => {
+        findTeamName();
+    }, [playerInfo]);
+
+    const saveEdits = () => {
+        console.log('save');
+        setEditing(false);
+        setInitialPlayerInfo(playerInfo);
+        if (props.savePlayer) {
+            props.savePlayer(playerInfo);
+        }
+    };
+
+    const cancelEdits = () => {
+        console.log('cancel');
+        setEditing(false);
+        setPlayerInfo(initalPlayerInfo);
+    };
+
+    const controlButtons = () => {
+        return (
+            editing ?
+                <div>
+                    <button onClick={() => cancelEdits()}>Cancel</button>
+                    <button onClick={() => saveEdits()}>Save</button>
+                </div> :
+                <button onClick={() => setEditing(true)}>Edit</button>
+        );
+    };
+
+    const editPlayer = (e) => {
+        console.log(e.target.value, e.target.name);
+        let player = { ...playerInfo };
+        if(e.target.name === 'team') {
+            player[e.target.name] = parseInt(e.target.value);
+        } else {
+            player[e.target.name] = e.target.value;
+        }
+        setPlayerInfo(player);
+        console.log(player);
+    };
+
+    const playerInfoElements = () => {
+        return (
+            editing ?
+                <div>
+                    <input placeholder='Position' type="text" name="position" value={playerInfo.position} onChange={(e) => editPlayer(e)} />
+                    <input placeholder='College' type="text" name="college" value={playerInfo.college} onChange={(e) => editPlayer(e)} />
+                    <select name="team" value={playerInfo.team} onChange={(e) => editPlayer(e)}>
+                        {props.teams.map((team) => {
+                            return <option key={team.id} value={team.id}>{team.name}</option>
+                        })}
+                    </select>
+                </div> :
+                <div>
+                    <div>{playerInfo.position}</div>
+                    <div>{playerInfo.college}</div>
+                    <div>{teamName}</div>
+                </div>
+        );
+    };
+
     return (
         <div style={{ ...styles.container, ...props.style }}>
-            <div style={styles.name}>{props.player.name}</div>
-            <img src={props.player.image} style={styles.playerImage} alt="player_image" />
-            <div>{props.player.teamName}</div>
+            {controlButtons()}
+            {editing ?
+                <input placeholder='Name' name="name" type="text" value={playerInfo.name} onChange={(e) => editPlayer(e)} />
+                :
+                <div style={styles.name}>{playerInfo.name}</div>}
+            <img src={ENDPOINT + playerInfo.image} style={styles.playerImage} alt="player_image" />
+            {playerInfoElements()}
         </div>
     );
 };
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -13,38 +13,53 @@ const Main = (props) => {
     const [searchText, setSearchText] = useState('');
     const [totalResults, setTotalResults] = useState(0);
 
+    const editPlayer = (player) => {
+        const url = ENDPOINT + 'players/' + player.id;
+        fetch(url, {
+            method: "PATCH",
+            body: JSON.stringify(player),
+            headers: {
+                "Content-Type": "application/json"
+            },
+        }).then(res => {
+            return res.ok ? res.text() : Promise.reject(res);
+        })
+        .then(responseText => console.log(responseText))
+        .catch(error => console.log(error));
+    };
+
     const fetchPlayers = (teams, page, searchText) => {
         let url = ENDPOINT + 'players?';
-        if(searchText) {
+        if (searchText) {
             url += 'q=' + searchText + "&";
         }
         url += '_page=' + page + '&_limit=' + PLAYER_LIMIT;
         console.log(url);
         fetch(url)
-        .then(res => {
-            if(res.ok) {
-                res.headers.forEach(function(val, key) { 
-                    console.log(key + ' -> ' + val); 
-                    if(key === 'x-total-count') {
-                        setTotalResults(val);
-                    }
-                });
-                return res.json();
-            } else {
-                return Promise.reject(res)
-            }
-        })
-        .then(players => initializePlayers(teams, players))
-        .catch(error => console.log(error));
+            .then(res => {
+                if (res.ok) {
+                    res.headers.forEach(function (val, key) {
+                        console.log(key + ' -> ' + val);
+                        if (key === 'x-total-count') {
+                            setTotalResults(val);
+                        }
+                    });
+                    return res.json();
+                } else {
+                    return Promise.reject(res)
+                }
+            })
+            .then(players => initializePlayers(teams, players))
+            .catch(error => console.log(error));
     };
 
     const fetchTeams = () => {
         fetch(ENDPOINT + 'teams')
-        .then(res => {
-            return res.ok ? res.json(): Promise.reject(res);
-        })
-        .then(teams => initializeTeams(teams))
-        .catch(error => console.log(error));
+            .then(res => {
+                return res.ok ? res.json() : Promise.reject(res);
+            })
+            .then(teams => initializeTeams(teams))
+            .catch(error => console.log(error));
     };
 
     const initializeTeams = (teams) => {
@@ -53,24 +68,7 @@ const Main = (props) => {
     };
 
     const initializePlayers = (teams, players) => {
-        console.log(players);
-
-        setPlayers(players.map((player) => {
-            let filteredTeams = teams.filter(t => {
-                return t.id === player.team
-            })
-            let team = null;
-            if(filteredTeams.length > 0) {
-                team = filteredTeams[0];
-            }
-
-            if(team != null) {
-                player.teamName = team.name;
-            } else {
-                player.teamName = 'No Team';
-            }
-            return player;
-        }));
+        setPlayers(players);
     };
 
     const previousPage = () => {
@@ -94,19 +92,19 @@ const Main = (props) => {
 
     const cardList = () => {
         return players.map((player) => {
-            let tempPlayer = {...player};
-            tempPlayer.image = ENDPOINT + player.image;
-            return <Card key={tempPlayer.name} player={tempPlayer}></Card>
+            let tempPlayer = { ...player };
+            // tempPlayer.image = ENDPOINT + player.image;
+            return <Card key={tempPlayer.name} player={tempPlayer} teams={storedTeams} savePlayer={editPlayer}></Card>
         });
     }
 
     return (
         <div style={{ ...styles.container, ...props.style }}>
             <div style={styles.title}>NBA Interview</div>
-            <Search style={styles.search} search={search}/>
+            <Search style={styles.search} search={search} />
             <div>
                 <button onClick={previousPage} disabled={pageNum <= 1}>Previous</button>
-                <button onClick={nextPage} disabled={pageNum*PLAYER_LIMIT >= totalResults}>Next</button>
+                <button onClick={nextPage} disabled={pageNum * PLAYER_LIMIT >= totalResults}>Next</button>
             </div>
             {cardList()}
         </div>
